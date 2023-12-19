d49c4d46ee6492f3b6a4bac1f6f14f5693a90d43
Nemphis7 <152176603+Nemphis7@users.noreply.github.com>
2023-12-19 21:24:17 +0100

    Update BookDayOne.py

--- BookDayOne.py
+++ BookDayOne.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 from datetime import datetime
 from sklearn.metrics.pairwise import cosine_similarity
+from io import BytesIO
 import requests
 
 def custom_format(value):
@@ -44,6 +45,12 @@ def load_stock_portfolio():
     try:
         url = 'https://raw.githubusercontent.com/Nemphis7/Pythonone/main/StockPortfolio.xlsx'
         stock_df = pd.read_excel(url, names=['Ticker', 'Quantity'])
+        stock_df['CurrentPrice'] = stock_df['Ticker'].apply(fetch_current_price)
+        stock_df.dropna(subset=['CurrentPrice'], inplace=True)
+        stock_df = stock_df[stock_df['CurrentPrice'] != 0]
+        stock_df['TotalValue'] = stock_df['Quantity'] * stock_df['CurrentPrice']
+        stock_df['CurrentPrice'] = stock_df['CurrentPrice'].round(2).apply(custom_format)
+        stock_df['TotalValue'] = stock_df['TotalValue'].round(2).apply(custom_format)
         return stock_df
     except Exception as e:
         st.error(f"Fehler beim Verarbeiten der Aktienportfolio-Datei: {e}")
@@ -86,7 +93,6 @@ def plot_financials(financial_df):
         plt.plot(category_df['YearMonth'].dt.to_timestamp(), category_df['AdjustedAmount'], marker='o', label=category)
     net_savings = financial_df.groupby('YearMonth')['AdjustedAmount'].sum()
     plt.plot(net_savings.index.to_timestamp(), net_savings.values, marker='o', label='Net Savings')
-    
     plt.title('Monthly Financial Overview')
     plt.xlabel('Month')
     plt.ylabel('Amount')
@@ -96,34 +102,26 @@ def plot_financials(financial_df):
 
 def kontenubersicht(df):
     st.title("Finanzdaten Analyse-App")
-
     aktueller_monat = datetime.now().strftime('%Y-%m')
     aktueller_monat_periode = pd.Period(aktueller_monat)
-
     if df is not None:
         df_sorted = df.sort_values(by='Datum', ascending=False)
         aktuelle_monatsdaten = df[df['Datum'].dt.to_period('M') == aktueller_monat_periode]
         aktuelle_monatsausgaben = aktuelle_monatsdaten[aktuelle_monatsdaten['Betrag'] < 0]['Betrag'].sum()
         aktuelle_monatseinnahmen = aktuelle_monatsdaten[aktuelle_monatsdaten['Betrag'] > 0]['Betrag'].sum()
-
         st.subheader(f"Ausgaben in {aktueller_monat}:")
         st.write(aktuelle_monatsausgaben)
-
         with st.expander("Letzte 10 Ausgaben anzeigen"):
             last_expenses = df_sorted[df_sorted['Betrag'] < 0].head(10)
             st.dataframe(last_expenses[['Datum', 'Name', 'Betrag']])
-
         st.subheader(f"Einnahmen in {aktueller_monat}:")
         st.write(aktuelle_monatseinnahmen)
-
         with st.expander("Letzte 10 Einnahmen anzeigen"):
             last_incomes = df_sorted[df_sorted['Betrag'] > 0].head(10)
             st.dataframe(last_incomes[['Datum', 'Name', 'Betrag']])
-
         gesamtausgaben = aktuelle_monatsdaten[aktuelle_monatsdaten['Betrag'] < 0]['Betrag'].sum()
         gesamteinnahmen = aktuelle_monatsdaten[aktuelle_monatsdaten['Betrag'] > 0]['Betrag'].sum()
         kontostand = gesamteinnahmen + gesamtausgaben
-
         st.subheader("Gesamtkontostand:")
         st.write(kontostand)
 
@@ -133,10 +131,9 @@ def show_add_ticker_form():
         ticker = st.text_input("Ticker")
         amount = st.number_input("Anzahl", min_value=1, step=1)
         submit_button = st.form_submit_button("Hinzufügen")
-
         if submit_button:
             url = 'https://raw.githubusercontent.com/Nemphis7/Pythonone/main/StockPortfolio.xlsx'
-            add_ticker_to_excel(ticker, amount, file_path)
+            add_ticker_to_excel(ticker, amount, "path_to_your_excel_file.xlsx")
 
 def add_ticker_to_excel(ticker, amount, file_path):
     try:
@@ -177,33 +174,97 @@ def empfehlung(df, stock_portfolio_df):
     st.subheader("Investment Recommendations:")
     st.dataframe(recommendations)
 
+def add_entry_to_excel(date, name, amount, file_path):
+    date_str = date.strftime('%d.%m.%Y')
+    new_entry = pd.DataFrame({
+        'Datum': [date_str], 
+        'Name': [name], 
+        'Betrag': [amount]
+    })
+    try:
+        df = pd.read_excel(file_path)
+        df['Datum'] = pd.to_datetime(df['Datum'], format='%d.%m.%Y', errors='coerce')
+        df = df.dropna(subset=['Datum'])
+        df['Datum'] = df['Datum'].dt.strftime('%d.%m.%Y')
+    except FileNotFoundError:
+        df = pd.DataFrame(columns=['Datum', 'Name', 'Betrag'])
+    df = pd.concat([df, new_entry], ignore_index=True)
+    df.to_excel(file_path, index=False)
+
+def custom_format_large_number(value):
+    if pd.isna(value):
+        return None
+    if isinstance(value, float):
+        value = round(value, 2)
+    return f"{value:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
+
+def display_fundamental_data(ticker, kurs):
+    kgv, market_cap, dividend_yield = get_fundamental_data(ticker)
+    formatierter_kurs = custom_format(kurs) if kurs is not None else 'N/A'
+    kgv = f"{kgv:.2f}" if isinstance(kgv, float) else kgv
+    market_cap = custom_format_large_number(market_cap) if market_cap != 'N/A' else 'N/A'
+    dividend_yield = f"{dividend_yield:.2f}%" if isinstance(dividend_yield, float) else dividend_yield
+    data = {
+        "Kennzahl": ["Aktueller Kurs", "KGV", "Marktkapitalisierung", "Dividendenrendite"],
+        "Wert": [formatierter_kurs, kgv, market_cap, dividend_yield]
+    }
+    df = pd.DataFrame(data)
+    st.table(df)
+
+def show_new_entry_form(df):
+    with st.form("new_entry_form", clear_on_submit=True):
+        st.subheader("Neue Buchung hinzufügen")
+        date = st.date_input("Datum", datetime.today())
+        name = st.text_input("Name")
+        amount = st.number_input("Betrag", step=1.0)
+        submitted = st.form_submit_button("Eintrag hinzufügen")
+        if submitted:
+            url = 'https://raw.githubusercontent.com/Nemphis7/Pythonone/main/Mappe1.xlsx'
+            add_entry_to_excel(date, name, amount, "path_to_your_excel_file.xlsx")
+            st.success("Buchung erfolgreich hinzugefügt.")
+            if 'load_data' in st.session_state:
+                del st.session_state['load_data']
+            df = load_data()
+    return df
+
+def plot_stock_history(ticker):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period="6mo")
+    if data.empty:
+        st.error(f"Keine Daten für {ticker} gefunden.")
+        return
+    plt.figure(figsize=(10, 5))
+    plt.plot(data.index, data['Close'], label='Schlusskurse')
+    plt.title(f"Aktienkursverlauf der letzten 6 Monate: {ticker}")
+    plt.xlabel('Datum')
+    plt.ylabel('Kurs in €')
+    plt.legend()
+    plt.grid(True)
+    st.pyplot(plt)
+
 def aktienkurse():
     st.title("Aktienkurse")
     aktien_name = st.text_input("Aktienname oder Tickersymbol eingeben:", "")
     if aktien_name:
         try:
-            kurs = fetch_current_price(aktien_name)
+            kurs = get_stock_data(aktien_name)
             if kurs is not None:
                 display_fundamental_data(aktien_name, kurs)
                 plot_stock_history(aktien_name)
             else:
-                st.error("Aktienkurs konnte nicht abgerufen werden.")
+                st.error("Aktienkurs konnte nicht abgerufen werden. Stellen Sie sicher, dass das Tickersymbol korrekt ist.")
         except Exception as e:
             st.error(f"Ein Fehler ist aufgetreten: {e}")
 
 def main():
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Wähle eine Seite", ["Kontenübersicht", "Analyse", "Empfehlung", "Aktienkurse"])
-
     st.title("Finanzdaten Analyse-App")
-
     if 'dataframe' not in st.session_state or page == "Kontenübersicht":
         st.session_state.dataframe = load_data()
-
     df = st.session_state.dataframe
     if df is not None:
         df = process_data(df)
-
     if page == "Kontenübersicht":
         kontenubersicht(df)
         df = show_new_entry_form(df)
