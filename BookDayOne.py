ea53b00a88167db8bf910d1542f8c8cf8ff3fc49
Momo-2711 <153088696+Momo-2711@users.noreply.github.com>
2024-02-01 14:36:12 +0100

    Update BookDayOne.py

--- BookDayOne.py
+++ BookDayOne.py
@@ -10,6 +10,13 @@ import numpy as np
 INFLATION_RATE = 0.02
 
 
+[theme]
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+secondaryBackgroundColor="#F0F2F6"
+textColor="#262730"
+font="sans serif"
+
 
 def custom_format(value):
     if pd.isna(value):
