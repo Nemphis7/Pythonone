5653d027823539ce2d935cc6a0977e5b66d7f89d
Momo-2711 <153088696+Momo-2711@users.noreply.github.com>
2024-02-04 23:59:48 +0100

    Update BookDayOne.py

--- BookDayOne.py
+++ BookDayOne.py
@@ -408,7 +408,6 @@ def account_overview(df, stock_df):
                 st.markdown("<br>", unsafe_allow_html=True)  # Add additional space if needed
     
 
-
    # Plot der Gesamtperformance am Ende der Account Overview
     if stock_df is not None:
         #st.subheader("Stocks in Portfolio:")
@@ -418,14 +417,14 @@ def account_overview(df, stock_df):
         html_stock_table = html_stock_table.replace('<table', '<table style="text-align: left;"')
         
         # Display the HTML table with Streamlit markdown
-        st.markdown(html_stock_table, unsafe_allow_html=True)
+        #st.markdown(html_stock_table, unsafe_allow_html=True)
 
     # Calculate and display the total portfolio value
     display_total_portfolio_value(stock_df)
     
     # Allow the user to select the time period for the historical data
     period = st.selectbox("Select the time period for the portfolio performance:",
-                          options=['5y', '3y', '1y', '6mo'], index=2)
+                          options=['5 y', '3 y', '1 y', '6 mo'], index=2)
     portfolio_history = get_portfolio_historical_data(stock_df, period)
     
     # Plot the historical data with Plotly
